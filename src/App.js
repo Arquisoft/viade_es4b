ef162704d8f54a778006663fd6225c31fdf530dc
PabloGlez <pabloarguelles97@gmail.com>
2020-02-13 16:46:30 +0100

    Aplicacion hola mundo en react

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+function HolaMundo(texto, años) {
+  var presentacion = <div>
+    <h2>Hola, soy {texto}</h2>
+    <h2>Tengo {años}</h2></div>
+  return presentacion;
+}
 function App() {
+  var nombre = "Pablo";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola
         </p>
+        {HolaMundo(nombre, 22)}
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React. Developed using Docker.
+          Learn React
         </a>
       </header>
     </div>
