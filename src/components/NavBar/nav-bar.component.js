f928ef09d749d2a16c3be7c3ee737062bb9f1302
PabloGlez <pabloarguelles97@gmail.com>
2020-02-27 00:50:21 +0100

    Modificada pagina de bienvenida

--- src/components/NavBar/nav-bar.component.js
+++ src/components/NavBar/nav-bar.component.js
@@ -58,7 +58,7 @@ const NavBar = (props: Props) => {
       <section className="header-wrap">
         <div className="logo-block">
           <Link to="/welcome">
-            <img src="/img/inrupt.svg" alt="inrupt" />
+            <img src="/img/logoNav.jpg" alt="logoNav" />
           </Link>
         </div>
 
