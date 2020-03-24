6b7d7237ae09f052c48f24861e8f17bcfd75c9fd
uo245020 <uo245020@uniovi.es>
2020-03-25 00:04:21 +0100

    Añadiendo tests

--- test/register.test.js
+++ test/register.test.js
@@ -16,6 +16,14 @@ describe.only('Register', () => {
         expect(container).toBeTruthy();
     });
 
+    test('renders with styled components', () => {
+        expect(getByTestId('register-wrapper')).toBeTruthy();
+        expect(getByTestId('panel-header')).toBeTruthy();
+        expect(document.querySelector('.register-panel')).toBeTruthy();
+        expect(document.querySelector('.panel-body')).toBeTruthy();
+        expect(document.querySelector('.actions')).toBeTruthy();
+      });
+
     test('renders title properly', () => {
         expect(getByTestId('title')).toBeTruthy();
     });
