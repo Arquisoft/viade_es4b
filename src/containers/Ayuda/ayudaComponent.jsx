226eefd0bdf807672cb88eeea3e915113cab081e
CarlosDeBenito <uo258559@uniovi.es>
2020-05-07 19:10:00 +0200

    Cambiados los videos de añadir una nueva ruta

--- src/containers/Ayuda/ayudaComponent.jsx
+++ src/containers/Ayuda/ayudaComponent.jsx
@@ -8,13 +8,16 @@ export default class AyudaComponent extends Component {
                 <h1 style={{ color: "black", margin: 10 }}>Guía de la aplicación</h1>
                 <h2 style={{ color: "black", margin: 10 }}>Como añadir un nueva ruta</h2>
 
-                <p style={{ color: "white", margin: 30 }}>Para añadir un nueva ruta el usuario deberá ir a la pestaña mapa, una vez ahi deberá pinchar en el mapa para indicar el primer punto de la ruta. Para seguir marcando puntos de la ruta el usuario deberá pinchar nuevamente en el mapa creando así puntos intermedios. Una vez seleccionado el primer punto se le abrirá un menú con la posibilidad de asociar imagenes a ese punto como por ejemplo una fuente que se encontrase en el camino. Asocie tantas imagenes como quiera para eso deberá pulsar el botón "Seleccionar archivo". Una vez elegida la la imagen deberá pulsar el boton "Enviar", y repetir esta acción tantas veces como imagenes quierá asociar a dicho punto. Cuando haya finalizado de añadir imagenes y puntos a la ruta pulsé el botón "Marcar ruta" para guardarla. Si desease ver la imagen que añadió al punto solamente deberá pulsa sobre su marcador y se mostrará la imagen.</p>
+                <p style={{ color: "white", margin: 30 }}>Para añadir un nueva ruta el usuario deberá ir a la pestaña mapa, una vez ahi podrá dar un nombre a la ruta y una descripción. Para porder marcar los puntos de la ruta deberá pinchar en el mapa para indicar el primer punto de esta. Para seguir marcando puntos de la ruta el usuario deberá pinchar nuevamente en el mapa creando así puntos intermedios. Una vez seleccionado el primer punto se le abrirá un menú con la posibilidad de asociar imagenes a ese punto como por ejemplo una fuente que se encontrase en el camino. Asocie tantas imagenes como quiera para eso deberá pulsar el botón "Seleccionar archivo". Una vez elegida la la imagen deberá pulsar el boton "Subir foto", y repetir esta acción tantas veces como imagenes quierá asociar a dicho punto. Cuando haya finalizado de añadir imagenes y puntos a la ruta pulsé el botón "Guardar ruta" para guardarla. Una vez guardada le aparecerá la nueva ruta en la pantalla con las rutas que ya tenía el usuario y aquellas que hayan sido compartidas por sus amigos.</p>
 
-                <p style={{ color: "white", margin: 30 }}>Aquí dejo un pequeño video que muestra como añadir varias rutas.</p>
+                <p style={{ color: "white", margin: 30 }}>Aquí dejo un pequeño video que muestra como una ruta con varios puntos.</p>
 
                 <div align="left" style={{ margin: 30 }}>
-                    <video src="./img/videoCrearRuta.mp4" controls width="500" height="400" className="video1"></video>
+                    <video src="./img/GuardarRuta.mp4" controls width="500" height="400" className="video1"></video>
                 </div>
+
+                <h2 style={{ color: "black", margin: 10 }}>Como visualizar tus rutas y las compartidas por tus amigos</h2>
+                <p style={{ color: "white", margin: 30 }}>Para visualizar sus rutas o las de sus amigos deberá ir a la pestaña mapa. Donde le aparecerán el nombre y la descripción de sus rutas y las compartidas por sus amigos. Para visualizarla solamente tendrá que pinchar sobre el nombre de aquella ruta que quiera ver.</p>
 
                 <h2 style={{ color: "black", margin: 10 }}>Como eliminar tus rutas guardadas</h2>
                 <p style={{ color: "white", margin: 30 }}>Para eliminar las rutas que el usuario tiene guardadas deberá ir a la pestaña mapa, una vez ahi debera dar al botón borrar rutas.</p>
